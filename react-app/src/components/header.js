c094672cc9a1bf98e07d55d92d77453113fad838
Gaku Ishikawa <gaku.jp.301@gmail.com>
2021-06-12 13:55:18 +0900

    h1

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -7,9 +7,9 @@ class Header extends React.Component {
       <header id="header">
         <div className="inner">
           {/* <a href="/" className="image avatar"><img src={Logo} alt="" /></a> */}
-          <h1><strong>Dox </strong> for every Animal<br />
-          一頭でも多く、彼らを救いたい。<br />
-          私たちはそれだけを願っています。</h1>
+          <h1><strong>Dox </strong> for every Animal</h1>
+          <h1>一頭でも多く、彼らを救いたい。</h1>
+          <h1>私たちはそれだけを願っています。</h1>
         </div>
       </header>
     );
