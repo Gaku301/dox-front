9662083efc6019d3bb4a558ba1bfe4ceb0b3f7f5
Gaku Ishikawa <gaku.jp.301@gmail.com>
2021-06-12 13:44:09 +0900

    bg変更

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     return (
       <header id="header">
         <div className="inner">
-          <a href="/" className="image avatar"><img src={Logo} alt="" /></a>
+          {/* <a href="/" className="image avatar"><img src={Logo} alt="" /></a> */}
           <h1><strong>Dox </strong> for every Animal<br />
           一頭でも多く、彼らを救いたい。<br />
           私たちはそれだけを願っています。</h1>
